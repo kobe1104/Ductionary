8682a5f25a782813599ef642360d6b5b8db8ec5e
Henry Hsu <henryhsu@Henrys-MacBook-Air.local>
2016-11-27 12:03:23 -0800

    ajax request working

--- content.js
+++ content.js
@@ -2,9 +2,10 @@
 // when double click on a word show it in a window
 document.addEventListener('dblclick', function() {
   var selectedStr = window.getSelection().toString();
-
-
-  div.textContent = selectedStr;
+  // make an ajax request to get definition
+  var definitionResponse = sendRequest(selectedStr);
+  console.log(definitionResponse);
+  div.textContent = definitionResponse;
   div.style.display = 'block';
 });
 
@@ -21,8 +22,23 @@ div.type = 'text/css';
 div.href = chrome.runtime.getURL('window.css');
 document.body.appendChild(div);
 
+// ajax request helper method
+// function sendRequest(word) {
+//   return ($.ajax({
+//     url: `http://api.urbandictionary.com/v0/define?term=` + word,
+//     dataType: 'json'
+//   }));
+// };
 
-
+function sendRequest(word) {
+  return ($.ajax({
+  url: 'https://mashape-community-urban-dictionary.p.mashape.com/define?term=' + word,
+  type: 'GET',
+  dataType: 'json',
+  beforeSend: function(xhr) {
+  xhr.setRequestHeader("X-Mashape-Authorization", "Iydop0yt1fmsh8dxQPnzMxhzHNV9p1z0MgujsnHaI9c61cj6Cc")}
+  }));
+}
 
 
 
