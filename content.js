a2bb633c523007881f9f47e8d692b86604caf6d6
Henry Hsu <henryhsu@Henrys-MacBook-Air.local>
2016-11-27 20:02:33 -0800

    double click word shows def!!!!

--- content.js
+++ content.js
@@ -1,47 +1,56 @@
 // alert("hello!");
+const TEST_RESPONSE = [
+  'a kind of fruit',
+  'something you eat',
+  "it's normally red",
+  'a major food in the world']
+
 // when double click on a word show it in a window
 document.addEventListener('dblclick', function() {
   var selectedStr = window.getSelection().toString();
   // make an ajax request to get definition
-  var definitionResponse = sendRequest(selectedStr);
-  console.log(definitionResponse);
-  div.textContent = definitionResponse;
-  div.style.display = 'block';
+  sendRequest(selectedStr);
+  // console.log(definitionResponse);
+  // console.log(definitionResponse);
+  // div.textContent = definitionResponse;
+  // div.style.display = 'block'; //
+  ul.style.display = 'block';
 });
+
 
 document.addEventListener('click', function() {
-    div.style.display = 'none';
+    // div.style.display = 'none';
+    ul.style.display = 'none';
+    while (ul.hasChildNodes()) {
+    ul.removeChild(ul.lastChild);
+    }
 });
 
+var ul = document.createElement('ul');
+ul.className = "def-popup";
+ul.rel = 'stylesheet';
+ul.type = 'text/css';
+ul.href = chrome.runtime.getURL('window.css');
+document.body.appendChild(ul);
 
-// window declearation
-var div = document.createElement('div');
-div.className = "def-popup";
-div.rel = 'stylesheet';
-div.type = 'text/css';
-div.href = chrome.runtime.getURL('window.css');
-document.body.appendChild(div);
 
-// ajax request helper method
-// function sendRequest(word) {
-//   return ($.ajax({
-//     url: `http://api.urbandictionary.com/v0/define?term=` + word,
-//     dataType: 'json'
-//   }));
-// };
+function fetchDefinitions(data) {
+  const definitions = data.list;
+  console.log(data.list);
+  for (i in definitions) {
+    var li = document.createElement('li');
+    li.textContent = definitions[i].definition;
+    ul.appendChild(li);
+  }
+}
 
-function sendRequest(word) {
+function sendRequest(word, success) {
   return ($.ajax({
   url: 'https://mashape-community-urban-dictionary.p.mashape.com/define?term=' + word,
   type: 'GET',
   dataType: 'json',
+  success: function(data) {fetchDefinitions(data)},
   beforeSend: function(xhr) {
   xhr.setRequestHeader("X-Mashape-Authorization", "Iydop0yt1fmsh8dxQPnzMxhzHNV9p1z0MgujsnHaI9c61cj6Cc")}
   }));
 }
-
-
-
-
-
-// chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerText);
--- note.md
+++ note.md
@@ -1,6 +1,3 @@
-1. Have content.js get the selected words and send it to background
-  and send it to background. Probably use "run_at" when
- click happens
-2. have background make the api call and get definition.
-3. have content.js render the definition. add "css" property to "content"
-  in manifest.
+1. add keydown event so window shows only when user double click while holding command key.
+2. list definitions and thumbs to rate.
+3. 
